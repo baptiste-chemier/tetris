9f90828eeae99082d1b972a9892022684b485119
Baptiste CHEMIER <Epulapp@EPULAPP55.univ-lyon1.fr>
2015-05-05 16:38:58 +0200

    Depart du projet.

---
+++ Demineur/src/demineur/Demineur.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demineur;
+
+import Vue.Vue;
+
+/**
+ *
+ * @author Epulapp
+ */
+public class Demineur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Vue fenetre = new Vue();
+        fenetre.setVisible(true);//On la rend visible
+    }
+    
+}
